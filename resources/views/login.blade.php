2ebdbce91211bfc78cb85d005dd226261b700ae1
Nestor Reverol <nestor.reverol092@gmail.com>
2018-08-23 14:10:14 +0200

    prototipo de login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,21 +3,22 @@
 @section('title','Inicio de Sesión')
 
 @section('maincontent')
-<div class="mx-auto text-center" style="width: 200px; margin-top: 10%;">
+<div class="mx-auto text-center" style="width: 40%; margin-top: 10%;">
   <form class="form-signin">
-      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <img class="mb-4" src="/images/logo.png" alt="" width="72" height="72">
+      <h6>Consulado General de la <strong>República Bolivariana de Venezuela</strong> en <strong>Barcelona Reino de España</strong></h3>
+      <h1 class="h3 mb-3 font-weight-normal">Por Favor Inicie Sesión</h1>
+      <label for="username" class="sr-only">Nombre de usuario</label>
+      <input type="text" id="username" class="form-control" placeholder="Nombre de usuario" required autofocus>
+      <label for="password" class="sr-only">Contraseña</label>
+      <input type="password" id="password" class="form-control" placeholder="Contraseña" required>
       <div class="checkbox mb-3">
         <label>
-          <input type="checkbox" value="remember-me"> Remember me
+          <input type="checkbox" value="remember-me"> Recuerdame
         </label>
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Ingresar <i class="fas fa-sign-in-alt"></i></button>
+      <p class="mt-5 mb-3 text-muted">&copy; ConsulVenBCN V2.0 - 2018</p>
     </form>
 </div>
 
