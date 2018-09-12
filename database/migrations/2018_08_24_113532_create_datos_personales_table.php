9c484ce8bf0b941da3f98896ec0fd10d00855464
Nestor Reverol <nestor.reverol092@gmail.com>
2018-09-12 11:57:17 +0200

    add migration tipo_documentos

--- database/migrations/2018_08_24_113532_create_datos_personales_table.php
+++ database/migrations/2018_08_24_113532_create_datos_personales_table.php
@@ -16,14 +16,14 @@ class CreateDatosPersonalesTable extends Migration
         Schema::create('datos_personales', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre1',25);
-            $table->string('nombre2',45);
+            $table->string('nombre2',45)->nullable();
             $table->string('apellido1',25);
-            $table->string('apellido2',45);
+            $table->string('apellido2',45)->nullable();
             $table->string('genero',1);
             $table->date('fecha_nacimiento');
             $table->string('lugar_nacimiento',70);
             $table->string('nacionalidad',50);
-            $table->integer('usuario_reg');
+            $table->integer('usuario_reg')->nullable();
             $table->timestamps();
         });
     }
